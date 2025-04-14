17d82258853f79f601736069be7525213559bb3b
Ravi Kasha <ravi.kasha@broadcom.com>
2025-04-14 12:54:10 +0530

    [VCFCON-13872]:Add credentials based authentication for service API invocation. (#13)

--- java/vcfa-samples-commons/src/main/java/com/vmware/vcfa/util/ConfigReader.java
+++ java/vcfa-samples-commons/src/main/java/com/vmware/vcfa/util/ConfigReader.java
@@ -1,14 +1,31 @@
 package com.vmware.vcfa.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
 import java.util.Map;
+
 import org.yaml.snakeyaml.Yaml;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 public class ConfigReader {
 
     Map<String, String> serverconfig;
 
-    public  ConfigReader() {
+    public ConfigReader() {
         String fileName = "application.yaml";  // File is under src/main/resources
         InputStream inputStream = ConfigReader.class.getClassLoader().getResourceAsStream(fileName);
         try {
@@ -21,8 +38,94 @@ public class ConfigReader {
     }
 
     public String getAccessToken() {
-        return serverconfig.get("access_token");
+        String acces_token = serverconfig.get("access_token");
+        boolean verifySsl = getVerifySsl();
+        if (acces_token.equals("null")) {
+            try {
+                acces_token = getAccessTokenWithSelfSignedCert(getServerUrl(), serverconfig.get("username"), serverconfig.get("password"), serverconfig.get("organization"), verifySsl, new FileInputStream(getSslCertPath()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return acces_token;
     }
+
+    public static String getAccessTokenWithSelfSignedCert(
+            String tmUrl,
+            String username,
+            String password,
+            String orgName,
+            boolean verifySSL,
+            InputStream sslCaCert) throws Exception {
+
+        if (verifySSL) {
+            // Load trusted cert from InputStream
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate) cf.generateCertificate(sslCaCert);
+
+            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+            ks.load(null, null);
+            ks.setCertificateEntry("ca", cert);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(ks);
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, tmf.getTrustManagers(), new java.security.SecureRandom());
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } else {
+            // Disable SSL verification completely (trust all certs + disable hostname check)
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Disable hostname verification
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        }
+
+        // Send POST request with basic auth
+        String urlStr = tmUrl + "/tm/cloudapi/1.0.0/sessions";
+        URL url = new URL(urlStr);
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        return getToken(username, password, orgName, conn);
+    }
+
+
+    private static String getToken(String username, String password, String orgName, HttpURLConnection conn) throws IOException {
+        String auth = String.format("%s@%s", username, orgName) + ":" + password;
+        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes());
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Authorization", "Basic " + encodedAuth);
+        conn.setRequestProperty("Accept", "*/*;version=41.0.0-alpha");
+
+        conn.connect();
+        try (OutputStream os = conn.getOutputStream()) {
+            os.write(new byte[0]);
+        }
+
+        String token = conn.getHeaderField("x-vmware-vcloud-access-token");
+        conn.disconnect();
+        return token;
+    }
+
 
     public String getServerUrl() {
         return serverconfig.get("url");
